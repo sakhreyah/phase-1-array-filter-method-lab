fe3c13f8f35da24aff44afac3b9faec5398f3eae
Musab Sakhreyah <mosabkhaled1998@gmail.com>
2023-10-05 10:26:54 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution here
+function findMatching(driversName, string) {
+    return driversName.filter(function (name) {
+        if (string.slice(1) == name.slice(1)) {
+            return name
+        }
+    })
+}
+
+function fuzzyMatch(driversName, string) {
+    return driversName.filter(function (name) {
+        if (string.slice(0, 1) == name.slice(0, 1)) {
+            return name
+        }
+    })
+}
+
+function matchName(driverObj, string) {
+    return driverObj.filter(function (Obj) {
+        if (Obj.name === string) {
+            return Obj
+        }
+
+    })
+}
